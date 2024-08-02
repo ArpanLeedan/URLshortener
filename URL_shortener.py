b27ea132cd1736e8c0778b3b3e299cb9c8d890bd
Arpan Basu <95604074+ArpanLeedan@users.noreply.github.com>
2024-08-02 21:35:00 +0530

    Update URL_shortener.py

--- URL_shortener.py
+++ URL_shortener.py
@@ -1,35 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 
-# account credentials
-username = "username of bitly" #enter your username
-password = "password" #enter your password
+app = Flask(__name__)
 
-# get the access token
-auth_res = requests.post("https://api-ssl.bitly.com/oauth/access_token", auth=(username, password))
-if auth_res.status_code == 200:
-    # if response is OK, get the access token
-    access_token = auth_res.content.decode()
-    print("[!] Got access token:", access_token)
-else:
-    print("[!] Cannot get access token, exiting...")
-    exit()
-# construct the request headers with authorization
-headers = {"Authorization": f"Bearer {access_token}"}
+BITLY_USERNAME = "arpan15.basu@gmail.com"
+BITLY_PASSWORD = 'Arpan@15basu'
 
-# get the group UID associated with our account
-groups_res = requests.get("https://api-ssl.bitly.com/v4/groups", headers=headers)
-if groups_res.status_code == 200:
-    # if response is OK, get the GUID
-    groups_data = groups_res.json()['groups'][0]
-    guid = groups_data['guid']
-else:
-    print("[!] Cannot get GUID, exiting...")
-    exit()
-# the URL you want to shorten
-url = input("enter the website link:- ")
-# make the POST request to get shortened URL for `url`
-shorten_res = requests.post("https://api-ssl.bitly.com/v4/shorten", json={"group_guid": guid, "long_url": url}, headers=headers)
-if shorten_res.status_code == 200:
-    # if response is OK, get the shortened URL
-    link = shorten_res.json().get("link")
-    print("Shortened URL:", link)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        url = request.form['url']
+
+        # get the access token
+        auth_res = requests.post("https://api-ssl.bitly.com/oauth/access_token", auth=(BITLY_USERNAME, BITLY_PASSWORD))
+        if auth_res.status_code == 200:
+            access_token = auth_res.content.decode()
+        else:
+            return render_template('index1.html', error="Cannot get access token, check your credentials.")
+
+        headers = {"Authorization": f"Bearer {access_token}"}
+
+        # get the group UID associated with our account
+        groups_res = requests.get("https://api-ssl.bitly.com/v4/groups", headers=headers)
+        if groups_res.status_code == 200:
+            groups_data = groups_res.json()['groups'][0]
+            guid = groups_data['guid']
+        else:
+            return render_template('index1.html', error="Cannot get GUID, please try again later.")
+
+        # make the POST request to get shortened URL for `url`
+        shorten_res = requests.post("https://api-ssl.bitly.com/v4/shorten", json={"group_guid": guid, "long_url": url}, headers=headers)
+        if shorten_res.status_code == 200:
+            link = shorten_res.json().get("link")
+            return render_template('index1.html', short_url=link)
+        else:
+            return render_template('index1.html', error="Failed to shorten the URL, please try again.")
+
+    return render_template('index1.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
